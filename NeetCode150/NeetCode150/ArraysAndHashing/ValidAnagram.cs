55bcbf94ad3181ed0ff4891c3d96f3ef8404e9c9
Deepak <deepakha1993@gmail.com>
2026-03-13 19:21:37 +0530

    Add solution for NeetCode #2 - Valid Anagram

---
+++ NeetCode150/NeetCode150/ArraysAndHashing/ValidAnagram.cs
@@ -0,0 +1,35 @@
+﻿
+namespace NeetCode150.ArraysAndHashing
+{
+    internal class ValidAnagram
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            s = s.ToLower();
+            t = t.ToLower();
+
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            int[] charCount = new int[26]; // [0, 0 , 0, ..., 0] (length 26)
+
+            for (int i = 0; i < s.Length; i++) // O(n)
+            {
+                charCount[s[i] - 'a']++; // Increment the count for the character in string s
+                charCount[t[i] - 'a']--; // Decrement the count for the character in string t
+            }
+
+            foreach (int count in charCount) // O(26) => O(1) (constant time)
+            {
+                if (count != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
--- NeetCode150/NeetCode150/Program.cs
+++ NeetCode150/NeetCode150/Program.cs
@@ -1,7 +1,29 @@
 ﻿using NeetCode150.ArraysAndHashing;
 
+
+#region 001 Contains Duplicate
+/* Executes the ContainsDuplicate code
+ 
 int[] nums = { 1, 2, 3, 1 };
 
 bool result = ContainsDuplicate.HasDuplicate(nums);
 
 Console.WriteLine(result);
+*/
+#endregion
+
+
+string s = "Silent";
+string t = "Listen";
+
+/* Handle space here in this edge case
+s = "Eleven plus two";
+t = "Twelve plus one"; 
+*/
+
+bool isAnagram = ValidAnagram.IsAnagram(s, t);
+
+if(isAnagram)
+    Console.WriteLine($"Yes.The two strings {s} and {t} are Anagrams.");
+else
+    Console.WriteLine($"No.The two strings {s} and {t} are not Anagrams.");
